3662eca54d2d7a09fce3f5bb60724c97024eae5c
mironovoc <mironbrrr1994@gmail.com>
2024-11-19 15:40:31 +0300

    Работа над ошибками

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,5 +1,8 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component  // Теперь Deth8 также является бином
 public class Deth8 {
 
 }
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -6,9 +6,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class Needle7 {
 
+    private Deth8 deth;  // Поле для инъекции
 
+    @Autowired
+    public Needle7(Deth8 deth) {  // Конструктор инъекции
+        this.deth = deth;
+    }
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( ";}
+        return ", смерть Кощея на игле :( ";
+    }
+
+    // Также можно добавить сеттер, чтобы показать возможность сеттерной инъекции
+    @Autowired
+    public void setDeth(Deth8 deth) {  // Сеттер инъекции
+        this.deth = deth;
+    }
 }
